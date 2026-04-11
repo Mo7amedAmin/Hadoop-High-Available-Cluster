099449e68461044e2943572a4ccbc5e215ea045e
Mohamed Ali <mohamedaminw22@gmail.com>
2026-04-11 14:29:45 +0200

    Edit config files for HA

--- shared/conf/hadoop/hadoop-env.sh
+++ shared/conf/hadoop/hadoop-env.sh
@@ -434,9 +434,10 @@ export JAVA_HOME=/usr/lib/jvm/java
 export HADOOP_HOME_WARN_SUPPRESS="TRUE"
 export HADOOP_ROOT_LOGGER="WARN,DRFA"
 
-
 export HDFS_NAMENODE_USER="root"
 export HDFS_DATANODE_USER="root"
+export HDFS_JOURNALNODE_USER="root"
+export HDFS_ZKFC_USER="root"
 export HDFS_SECONDARYNAMENODE_USER="root"
 export YARN_RESOURCEMANAGER_USER="root"
 export YARN_NODEMANAGER_USER="root"
